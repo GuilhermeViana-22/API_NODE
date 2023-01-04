34d8441c5a5b2750975c092cc175ed011bfd2582
Guilherme Viana <gguicido.viana@gmail.com>
2023-01-03 23:45:03 -0300

    ajustes na api node

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
-
+return res.json({mensagem: 'deu good'});
 });
 
+//caminho de especificação
+///http://localhost:3000/
 server.listen(3000, () =>{
     console.log('Servidor Funcionando')
 });
